c858b8a4021fe808f1e5b90146b8a193d73f20a8
Tom Scharstein <trscharstein@gmail.com>
2023-02-20 18:19:51 -0700

    Basic template generation for POC

---
+++ README.md
@@ -0,0 +1,9 @@
+# minion help pages
+
+Dynamically retrieving help pages inside a CEP panel from a Github repo. Why update a website with help docs when the panel can update it's own help section in real-time without needing any updates or reinstallation?
+
+## Usage
+
+```
+npm run convert [name of folder]
+```
--- lib/convert.ts
+++ lib/convert.ts
@@ -5,11 +5,34 @@ interface fileObj {
   index: number;
   title: string;
   content: string;
+  uuid: string;
 }
 
+if (process.argv.length <= 2) {
+  console.error(
+    "Must include name of folder after npm run convert command:",
+    "npm run convert foo"
+  );
+}
+const fns = require("date-fns");
+const targName = process.argv[process.argv.length - 1];
+
 const init = async (): Promise<void> => {
-  let pageList = await getPagesArray("./pages");
-  console.log(pageList);
+  let pageList = await getPagesArray(`./${targName}`);
+  await lib.writeFile(
+    `./results/${targName}.json`,
+    JSON.stringify({
+      pages: pageList,
+      timestamp: Date.now(),
+      date: {
+        short: fns.format(new Date(), `MM/dd/yy`),
+        long: fns.format(new Date(), `eeee, LLL do`),
+        time: fns.format(new Date(), `h:mma`),
+      },
+      name: targName,
+    })
+  );
+  console.log(`All done 👍`);
 };
 
 const getPagesArray = async (filepath: string): Promise<any[]> => {
@@ -20,7 +43,6 @@ const getPagesArray = async (filepath: string): Promise<any[]> => {
     return Number(a.match(/^\d{1,}/)[0]) - Number(b.match(/^\d{1,}/)[0]);
   });
   for (let file of list) {
-    console.log(file);
     let temp = {} as fileObj;
     //@ts-ignore
     temp["index"] = Number(file.match(/^\d{1,}/)[0]);
@@ -28,6 +50,7 @@ const getPagesArray = async (filepath: string): Promise<any[]> => {
       .replace(/\.md$/, "")
       .match(/(?<=\W)[\w\s]*$/)[0]
       .trim();
+    temp["uuid"] = generateQuickGuid();
     temp["content"] = await lib.readFile(
       p.join(p.resolve(filepath), file),
       false
@@ -36,5 +59,13 @@ const getPagesArray = async (filepath: string): Promise<any[]> => {
   }
   return result;
 };
+
+// https://stackoverflow.com/a/13403498/10307226
+function generateQuickGuid(): string {
+  return (
+    Math.random().toString(36).substring(2, 4) +
+    Math.random().toString(36).substring(2, 4)
+  );
+}
 
 init();
